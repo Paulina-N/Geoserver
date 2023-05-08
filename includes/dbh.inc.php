9baf19fa79b58610840c1418ccdf05b9668329bd
Paulina-N <narkunaitepaulina@gmail.com>
2023-05-08 21:45:57 +0300

    connect Heroku DB

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,13 +1,25 @@
 <?php
 // include $_SERVER['DOCUMENT_ROOT'] . '/map_pwd.php';
 
-$serverName = "localhost";
-$dBUserName = "map";
-// $dBPassword = $DBPWD;
-$dBPassword = "map"; //password temporary shown for demonstrational purposes
-$dBName = "map";
+//Heroku ClearDB connection
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-$conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+// local server
+// $serverName = "localhost";
+// $dBUserName = "map";
+// // $dBPassword = $DBPWD;
+// $dBPassword = "map"; //password temporary shown for demonstrational purposes
+// $dBName = "map";
+
+// $conn = mysqli_connect($serverName, $dBUserName, $dBPassword, $dBName);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
